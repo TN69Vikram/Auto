0260e8395a6837cc631580b5c17b503922f64e33
UHD4K <148746604+UHD4k@users.noreply.github.com>
2024-05-04 11:15:46 +0530

    Update info.py

--- info.py
+++ info.py
@@ -50,8 +50,8 @@ FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '-100
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://jdofficial143:jayaraman143@cluster0.b7w8f3d.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "jdofficial143")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://TN-AutoFilterBot:kIA2sy9zhqDLHngm@cluster0.mdmmnn3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_NAME = environ.get('DATABASE_NAME', "TN-AutoFilterBot")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'techvjcollection')
 
 # Premium And Referal Settings
